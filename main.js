9523b968bb78dfdfb995c8916148fc116855c76a
Samrat Pudasaini <85990839+samratiam@users.noreply.github.com>
2022-02-25 17:45:07 +0545

    Calculate Human Age in Dog Years

---
+++ main.js
@@ -0,0 +1,35 @@
+//Store my age in variable myAge
+var myAge = 25;
+
+//First two years of dog count as 10.5 dog years each
+earlyYears = 2;
+earlyYears *= 10.5
+
+//Subtract 2 from myAge as we already compute for early 2years
+laterYears = myAge - 2;
+
+// Each year following equates to 4 dog years after first 2years
+laterYears *= 4
+
+console.log(`Early 2years of myAge in Dog years: ${earlyYears}`);
+console.log(`After 2years of myAge in Dog years: ${laterYears}`);
+
+//Calculating myAge in Dog Years
+myAgeinDogYears = earlyYears + laterYears;
+
+//Convert myName into lowercase
+var myName = "Samrat";
+myName = myName.toLowerCase();
+
+//Display myAge in Dog Years
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeinDogYears} years old in dog years.`);
+
+
+
+
+
+
+
+
+
+
